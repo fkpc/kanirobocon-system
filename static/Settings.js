1712a3486ab4c7d90902d53c4a835b1e7243daad
Taisuke Fukuno <fukuno@jig.jp>
2024-01-07 19:33:10 +0900

    Matz葉がにロボコン設定

--- static/Settings.js
+++ static/Settings.js
@@ -1,12 +1,12 @@
 class Settings {
 	static getTitle() {
-		return "越前がにロボコン";
+		return "Matz葉がにロボコン";
 	};
 	static getGroup(category) {
-		return category == "T" ? "エントリー部門" : "歩行部門";
+		return category == "T" ? "小学生部門" : "オープン部門";
 	}
 	static getNumberOfMatch(category) {
-		return category == "T" ? 8 : 4;
+		return category == "T" ? 8 : 8;
 	}
 	static getNumberOfStage(category) {
 		return category == "T" ? 2 : 1;
